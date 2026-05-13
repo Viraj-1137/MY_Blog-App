2126dd18824bf2f3546e0360aad2a1d3a561ea13
Viraj Kadam <kadamviraj599@gmail.com>
2026-05-13 18:10:15 +0530

    changed database configuration

--- Blog_app/settings.py
+++ Blog_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 
 load_dotenv()
@@ -85,6 +86,14 @@ WSGI_APPLICATION = 'Blog_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
+
 
 CLOUDINARY_STORAGE = {
 
